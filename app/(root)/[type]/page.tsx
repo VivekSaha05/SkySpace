7ce4b03f4792f1624fa78bab81e6eee771840b67
Vivek Saha <viveksahapop@gmail.com>
2024-11-13 20:51:47 +0530

    Commit 7 (probly final)
    Added Dashboard and done some minor changes in UI

--- app/(root)/[type]/page.tsx
+++ app/(root)/[type]/page.tsx
@@ -1,99 +1,49 @@
-import Image from "next/image";
-import Link from "next/link";
+import React from "react";
+import Sort from "@/components/Sort";
+import { getFiles } from "@/lib/actions/file.actions";
 import { Models } from "node-appwrite";
+import Card from "@/components/Card";
+import { getFileTypesParams } from "@/lib/utils";
 
-import ActionDropdown from "@/components/ActionDropdown";
-import { FormattedDateTime } from "@/components/FormattedDateTime";
-import { Thumbnail } from "@/components/Thumbnail";
-import { Separator } from "@/components/ui/separator";
-import { getFiles, getTotalSpaceUsed } from "@/lib/actions/file.actions";
-import { convertFileSize, getUsageSummary } from "@/lib/utils";
+const Page = async ({ searchParams, params }: SearchParamProps) => {
+  const type = ((await params)?.type as string) || "";
+  const searchText = ((await searchParams)?.query as string) || "";
+  const sort = ((await searchParams)?.sort as string) || "";
 
-const Dashboard = async () => {
-  // Parallel requests
-  const [files, totalSpace] = await Promise.all([
-    getFiles({ types: [], limit: 10 }),
-    getTotalSpaceUsed(),
-  ]);
+  const types = getFileTypesParams(type) as FileType[];
 
-  // Get usage summary
-  const usageSummary = getUsageSummary(totalSpace);
+  const files = await getFiles({ types, searchText, sort });
 
   return (
-    <div className="dashboard-container">
-      <section>
+    <div className="page-container">
+      <section className="w-full">
+        <h1 className="h1 capitalize">{type}</h1>
 
-        {/* Uploaded file type summaries */}
-        <ul className="dashboard-summary-list">
-          {usageSummary.map((summary) => (
-            <Link
-              href={summary.url}
-              key={summary.title}
-              className="dashboard-summary-card"
-            >
-              <div className="space-y-4">
-                <div className="flex justify-between gap-3">
-                  <Image
-                    src={summary.icon}
-                    width={100}
-                    height={100}
-                    alt="uploaded image"
-                    className="summary-type-icon"
-                  />
-                  <h4 className="summary-type-size">
-                    {convertFileSize(summary.size) || 0}
-                  </h4>
-                </div>
+        <div className="total-size-section">
+          <p className="body-1">
+            Total: <span className="h5">0 MB</span>
+          </p>
 
-                <h5 className="summary-type-title">{summary.title}</h5>
-                <Separator className="bg-light-400" />
-                <FormattedDateTime
-                  date={summary.latestDate}
-                  className="text-center"
-                />
-              </div>
-            </Link>
+          <div className="sort-container">
+            <p className="body-1 hidden text-light-200 sm:block">Sort by:</p>
+
+            <Sort />
+          </div>
+        </div>
+      </section>
+
+      {/* Render the files */}
+      {files.total > 0 ? (
+        <section className="file-list">
+          {files.documents.map((file: Models.Document) => (
+            <Card key={file.$id} file={file} />
           ))}
-        </ul>
-      </section>
-
-      {/* Recent files uploaded */}
-      <section className="dashboard-recent-files">
-        <h2 className="h3 xl:h2 text-light-100">Recent files uploaded</h2>
-        {files.documents.length > 0 ? (
-          <ul className="mt-5 flex flex-col gap-5">
-            {files.documents.map((file: Models.Document) => (
-              <Link
-                href={file.url}
-                target="_blank"
-                className="flex items-center gap-3"
-                key={file.$id}
-              >
-                <Thumbnail
-                  type={file.type}
-                  extension={file.extension}
-                  url={file.url}
-                />
-
-                <div className="recent-file-details">
-                  <div className="flex flex-col gap-1">
-                    <p className="recent-file-name">{file.name}</p>
-                    <FormattedDateTime
-                      date={file.$createdAt}
-                      className="caption"
-                    />
-                  </div>
-                  <ActionDropdown file={file} />
-                </div>
-              </Link>
-            ))}
-          </ul>
-        ) : (
-          <p className="empty-list">No files uploaded</p>
-        )}
-      </section>
+        </section>
+      ) : (
+        <p className="empty-list">No files uploaded</p>
+      )}
     </div>
   );
 };
 
-export default Dashboard;
+export default Page;
